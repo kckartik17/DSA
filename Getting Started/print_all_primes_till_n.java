6ee664bcb6a930d8f1fe6da030726bec8ab34d86
Kartik Chauhan <kckartik17@gmail.com>
2020-08-30 13:01:57 +0530

    Print all prime number till N
    Signed-off-by: Kartik Chauhan <kckartik17@gmail.com>

---
+++ Getting Started/print_all_primes_till_n.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class print_all_primes_till_n {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int low = scn.nextInt();
+        int high = scn.nextInt();
+        for (; low <= high; low++) {
+            int count = 0;
+            for (int div = 2; div * div <= low; div++) {
+                if (low % div == 0) {
+                    count++;
+                    break;
+                }
+            }
+
+            if (count == 0) {
+                System.out.println(low);
+            }
+        }
+        scn.close();
+    }
+}
